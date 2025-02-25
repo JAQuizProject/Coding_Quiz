465e6a79189ba9b274fbc95a247495678e81065c
JAEHO AHN <ajh4234@gmail.com>
2025-02-25 17:47:19 +0900

    initial commit

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching API:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Coding Quiz</h1>
+      <p>Backend says: {message}</p>
+    </div>
+  );
+}
